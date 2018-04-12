ebd27087d1fa5e5ae7506d37797b5033312e5762
Vlad Rindevich <rindevich.vs@gmail.com>
2018-04-12 21:14:58 +0700

    build(core): debabilify es build

--- packages/core/.babelrc.js
+++ packages/core/.babelrc.js
@@ -1,24 +1,28 @@
-const { NODE_ENV, BABEL_ENV } = process.env
+const { NODE_ENV, BABEL_ENV, NOBABEL } = process.env
 
 const cjs = BABEL_ENV === 'cjs' || NODE_ENV === 'test'
 const prod = NODE_ENV === 'production'
+const nobabel = NOBABEL === 'true'
 
 module.exports = {
   presets: [
-    [
-      '@babel/env',
-      {
-        loose: true,
-        modules: false,
-        exclude: ['transform-typeof-symbol'],
-        forceAllTransforms: true,
-      },
-    ],
     '@babel/react',
-    '@babel/stage-2',
+    ...nobabel ? [] : [
+      [
+        '@babel/env',
+        {
+          loose: true,
+          modules: false,
+          exclude: ['transform-typeof-symbol'],
+          forceAllTransforms: true,
+        },
+      ],
+      '@babel/stage-2',
+    ],
   ],
   plugins: [
     cjs && '@babel/transform-modules-commonjs',
+    nobabel && '@babel/plugin-syntax-object-rest-spread',
     'annotate-pure-calls'
   ].filter(Boolean),
 }
